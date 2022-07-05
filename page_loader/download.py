9a9ece73bf82fe6336642b50177d84bcf0ad2d13
Sergey Golodyaev <gabadysergei@yandex.ru>
2022-07-05 14:35:38 +0300

    step1 upgrade python

--- page_loader/download.py
+++ page_loader/download.py
@@ -3,15 +3,74 @@ import re
 
 import requests
 
+from urllib.parse import urljoin, urlparse
+from bs4 import BeautifulSoup
+
 DEFAULT_DST_FOLDER = os.getcwd()
 
 
-def download(source: str, destination: str = DEFAULT_DST_FOLDER) -> str:
-    name_file = source.split('//')[-1]
-    name_local = '{0}{1}'.format(re.sub(r'\W', '-', name_file), '.html')
-    request = requests.get(source, stream=True)
-    with open(os.path.join(destination, name_local), 'wb') as filename:
-        for chunk in request.iter_content():
-            filename.write(chunk)
+def check_domain(outer_host: str, inner_host: str):
+    """Check local assets for local domain."""
+    url_parts = urlparse(inner_host)
+    return url_parts.hostname is None or outer_host in url_parts.hostname
 
-    return os.path.join(destination, name_local)
+
+def parse_url(url: str):
+    """Split url on host and path."""
+    url_parts = urlparse(url)
+    return (url_parts.hostname, url_parts.path[1:])
+
+
+def make_name(url):
+    """Make name for assets."""
+    hostname, path = parse_url(url)
+    name, extension = os.path.splitext(path)
+    name = re.sub(r'\W', '-', os.path.join(hostname, name))
+    extension = extension if extension else '.html'
+    return '{0}{1}'.format(name, extension)
+
+
+def make_folder(filename: str, dst: str):
+    """Create folder for assets and return folder name."""
+    folder = filename.replace('.html', '_files')
+    os.mkdir(os.path.join(dst, folder))
+    return folder
+
+
+def download_assets(src: str, dst: str):
+    """Download assets for page."""
+    req = requests.get(src, stream=True)
+    assert_name = make_name(src)
+    with open(os.path.join(dst, assert_name), 'wb') as filename:
+        filename.write(req.content)
+    return assert_name
+
+
+def parse_html(url: str, dst: str, html: str):
+    """Parse html file."""
+    html_name = make_name(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    assets = (
+        asset
+        for asset in soup.find_all('img')
+        if check_domain(url, asset.get('src'))
+    )
+
+    if assets:
+        assets_dst = make_folder(html_name, dst)
+        for asset in assets:
+            asset_name = download_assets(
+                urljoin(url, asset.get('src')),
+                os.path.join(dst, assets_dst),
+            )
+            asset['src'] = os.path.join(assets_dst, asset_name)
+    return html_name, soup.prettify()
+
+
+def download(src: str, dst: str = DEFAULT_DST_FOLDER):
+    """Download html page to dst folder."""
+    request = requests.get(src)
+    html_name, html_data = parse_html(src, dst, request.text)
+    with open(os.path.join(dst, html_name), 'w') as filename:
+        filename.write(html_data)
+    return os.path.join(dst, html_name)
